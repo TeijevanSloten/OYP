56eb55d5b05118ac6b159d9fa24b180618d98411
reneberghuis <reneberghuis@hotmail.com>
2015-02-09 13:52:41 +0100

    changed attachments folder

--- outlook/src/main/java/com/ordina/servlet/ControllerServlet.java
+++ outlook/src/main/java/com/ordina/servlet/ControllerServlet.java
@@ -76,7 +76,7 @@ public class ControllerServlet extends HttpServlet {
                                 for (int i = 0; i < multiPart.getCount(); i++) {
                                     MimeBodyPart part = (MimeBodyPart) multiPart.getBodyPart(i);
                                     if (Part.ATTACHMENT.equalsIgnoreCase(part.getDisposition())) {
-                                        part.saveFile("c://Attachments/" + part.getFileName());
+                                        part.saveFile("d://projecten/Attachments/" + part.getFileName());
                                     }
                                 }
 
