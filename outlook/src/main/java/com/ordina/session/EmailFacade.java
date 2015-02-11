aabf98af1ff5ecd56f3184f2ceb50b9354a7235f
Teije van Sloten <teije-vs@hotmail.com>
2015-02-11 13:34:34 +0100

    little changes made to improve sorting and showing the email.

--- outlook/src/main/java/com/ordina/session/EmailFacade.java
+++ outlook/src/main/java/com/ordina/session/EmailFacade.java
@@ -2,6 +2,7 @@ package com.ordina.session;
 
 import com.ordina.entity.Email;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -12,6 +13,13 @@ public class EmailFacade extends AbstractFacade<Email> {
     @PersistenceContext(unitName = "com.ordina_Test3_war_1.0-SNAPSHOTPU")
     private EntityManager em;
 
+    private static final Comparator<Email> ORDER_BY_DATE = 
+                                        new Comparator<Email>() {
+            public int compare(Email e1, Email e2) {
+                return e2.getDate().compareTo(e1.getDate());
+            }
+    };
+    
     @Override
     protected EntityManager getEntityManager() {
         return em;
@@ -27,7 +35,7 @@ public class EmailFacade extends AbstractFacade<Email> {
     
     public List<Email> findAllByDate() {
         List<Email> emails = getEntityManager().createNamedQuery("Email.findAll", Email .class).getResultList();
-        Collections.reverse(emails);
+         Collections.sort(emails, ORDER_BY_DATE);
         return emails;
     }
 }
